a02159cf24e3c0fcc2af80fe77c10a335038957e
Luca Di Fazio <highliuk@gmail.com>
2021-10-19 15:52:15 +0200

    Full Stack Build

--- server/index.js
+++ server/index.js
@@ -10,9 +10,15 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(express.json())
 app.use(cors())
+app.use(express.static(__dirname + "/public/"))
 
 // API routes
 app.use("/api/posts", PostRouter)
+
+// SPA
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
+})
 
 // Error Middleware
 app.use(errorMiddleware)
